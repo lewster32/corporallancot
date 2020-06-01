c792ade02afa2ad8037b42913fd06cbff12b8791
Chris Sebok <chris@shadowmoses.co.uk>
2020-06-01 09:39:30 +0100

    Basic dockerisation & minor improvements
    - Added and configured docker-compose.yml to include basic MariaDB setup
    - Added Dockerfile & .diockerignore for building the Node.js Bot host image
    - Updated README.md to detail bot setup instructions
    - Deleted config-example.json as example now resides in README.md
    - Fixed a bug in the notes table setup promise wrapper handling
    - Added / improved some logging
    - Improved error handling slightly; removed some unhelpful try-catch statements & removed bot.init() call from constructor -> moved it out into an asyncronous call from app.js
    - Removed *some* of the exclaimations and ellipses from log messages 8==============D
    - Added a shared .code-workspace file to avoid formatting faux pas
    - Bot should now work "out of the box" with no installs / configuration required other than those in README.md

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const Bot = require("./app/bot");
 const fs = require("fs");
-console.log("Checking config...");
+console.log("Checking config");
 const config = JSON.parse(fs.readFileSync("config.json"));
 if (!config) {
-  throw new Error("Config not found or unreadable!");
+  throw new Error("Config not found or unreadable");
 }
-console.log("Config loaded!");
+console.log("Config loaded");
 
 const bot = new Bot(config);
+
+(async () => {
+  await bot.init();
+})();
