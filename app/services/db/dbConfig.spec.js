016665bd5c34c7800f7c5113a3cadf418f54fc1d
Chris Sebok <chris@shadowmoses.co.uk>
2020-06-21 11:08:37 +0100

    WIP #19 and #18 - Moved all sensitive info out of config.json so we can now push it to the repository

--- app/services/db/dbConfig.spec.js
+++ app/services/db/dbConfig.spec.js
@@ -3,73 +3,85 @@
 const DbConfig = require("./dbConfig");
 
 describe("dbConfig", function () {
-  it("maps injected appConfig.db property to .database", function () {
+  it("maps injected appConfig.database.name property to .database", function () {
     const appConfig = {
-      db: "test_database"
+      database: {
+        name: "test_database"
+      }
     };
     const dbConfig = new DbConfig({ appConfig: appConfig });
-    expect(dbConfig.database).toBe(appConfig.db);
+    expect(dbConfig.database).toBe(appConfig.database.name);
   });
 
-  it("maps injected appConfig.dbHost property to .server", function () {
+  it("maps injected appConfig.database.server property to .server", function () {
     const appConfig = {
-      dbHost: "test_server"
+      database: {
+        server: "test_server"
+      }
     };
     const dbConfig = new DbConfig({ appConfig: appConfig });
-    expect(dbConfig.server).toBe(appConfig.dbHost);
+    expect(dbConfig.server).toBe(appConfig.database.server);
   });
 
-  it("maps injected appConfig.dbUser property to .username", function () {
+  it("maps injected appConfig.database.user property to .username", function () {
     const appConfig = {
-      dbUser: "test_user"
+      database: {
+        user: "test_user"
+      }
     };
     const dbConfig = new DbConfig({ appConfig: appConfig });
-    expect(dbConfig.username).toBe(appConfig.dbUser);
+    expect(dbConfig.username).toBe(appConfig.database.user);
   });
 
-  it("maps injected appConfig.dbPassword property to .password", function () {
+  it("maps injected appConfig.database.password property to .password", function () {
     const appConfig = {
-      dbPassword: "test_password"
+      database: {
+        password: "test_password"
+      }
     };
     const dbConfig = new DbConfig({ appConfig: appConfig });
-    expect(dbConfig.password).toBe(appConfig.dbPassword);
+    expect(dbConfig.password).toBe(appConfig.database.password);
   });
 
-  it("maps injected appConfig.dbConnectionRetryCount property to .connRetryCount", function () {
+  it("maps injected appConfig.database.connectionRetryCount property to .connRetryCount", function () {
     const appConfig = {
-      dbConnectionRetryCount: 3
+      database: {
+        connectionRetryCount: 3
+      }
     };
     const dbConfig = new DbConfig({ appConfig: appConfig });
-    expect(dbConfig.connRetryCount).toBe(appConfig.dbConnectionRetryCount);
+    expect(dbConfig.connRetryCount).toBe(appConfig.database.connectionRetryCount);
   });
 
-  it("maps injected appConfig.dbConnectionRetryDelay property to .connRetryDelay", function () {
+  it("maps injected appConfig.database.connectionRetryDelay property to .connRetryDelay", function () {
     const appConfig = {
-      dbConnectionRetryDelay: 3000
+      database: {
+        connectionRetryDelay: 3000
+      }
     };
     const dbConfig = new DbConfig({ appConfig: appConfig });
-    expect(dbConfig.connRetryDelay).toBe(appConfig.dbConnectionRetryDelay);
+    expect(dbConfig.connRetryDelay).toBe(appConfig.database.connectionRetryDelay);
   });
 
-  it("uses default value of 'localhost' for .server if appConfig.dbHost is empty", function () {
+  it("uses default value of 'localhost' for .server if appConfig property is empty", function () {
     const appConfig = {};
     const dbConfig = new DbConfig({ appConfig: appConfig });
     expect(dbConfig.server).toBe("localhost");
   });
 
-  it("uses default value of '10' for .connRetryCount if appConfig.dbConnectionRetryCount is empty", function () {
+  it("uses default value of '10' for .connRetryCount if appConfig property is empty", function () {
     const appConfig = {};
     const dbConfig = new DbConfig({ appConfig: appConfig });
     expect(dbConfig.connRetryCount).toBe(10);
   });
 
-  it("uses default value of '5000' for .connRetryDelay if appConfig.dbConnectionRetryDelay is empty", function () {
+  it("uses default value of '5000' for .connRetryDelay if appConfig property is empty", function () {
     const appConfig = {};
     const dbConfig = new DbConfig({ appConfig: appConfig });
     expect(dbConfig.connRetryDelay).toBe(5000);
   });
 
-  it("uses default value of 'notes' for .database if appConfig.db is empty", function () {
+  it("uses default value of 'notes' for .database if appConfig property is empty", function () {
     const appConfig = {};
     const dbConfig = new DbConfig({ appConfig: appConfig });
     expect(dbConfig.database).toBe("notes");
